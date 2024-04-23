63449d9db125ed85bd6e9dc516b36f364981a988
David Attias <best.trading.indicator@gmail.com>
2024-04-23 12:21:53 +0200

    various Stripe config

--- config.js
+++ config.js
@@ -21,8 +21,8 @@ const config = {
         // REQUIRED — we use this to find the plan in the webhook (for instance if you want to update the user's credits based on the plan)
         priceId:
           process.env.NODE_ENV === "development"
-            ? "price_1Niyy5AxyNprDp7iZIqEyD2h"
-            : "price_456",
+            ? "price_1P8gHBAecxHCE0q85Dh4XvGc"
+            : "price_1P8gHBAecxHCE0q85Dh4XvGc",
         //  REQUIRED - Name of the plan, displayed on the pricing page
         name: "Starter",
         // A friendly description of the plan, displayed on the pricing page. Tip: explain why this plan and not others
@@ -45,8 +45,8 @@ const config = {
         isFeatured: true,
         priceId:
           process.env.NODE_ENV === "development"
-            ? "price_1O5KtcAxyNprDp7iftKnrrpw"
-            : "price_456",
+            ? "price_1P8gJCAecxHCE0q8gNjOJDyQ"
+            : "price_1P8gJCAecxHCE0q8gNjOJDyQ",
         name: "Advanced",
         description: "You need more power",
         price: 39,
